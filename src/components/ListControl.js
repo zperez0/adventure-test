3c1b501fb5ef708e9c186bb8aab433eba9f62507
Yazmin Perez <zoeperez333@gmail.com>
2022-09-14 13:05:20 -0700

    move app.js & app.css to components

---
+++ src/components/ListControl.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+
+function ListControl() {
+  const [lists, setLists] = useState()
+  
+
+
+
+  return (
+    <div>ListControl</div>
+  )
+}
+
+export default ListControl
