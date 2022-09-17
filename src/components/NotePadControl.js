bc404047ae1c85f3c4d5c5f8c87fa15e47e44ada
Yazmin Perez <zoeperez333@gmail.com>
2022-09-16 20:29:50 -0700

    setup class component

--- src/components/NotePadControl.js
+++ src/components/NotePadControl.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import NewNotePadForm from "./NewNotePadForm";
+import NotePadList from "./NotePadList";
 
-function NotePadControl() {
-  return (
-    <div>NotePadControl</div>
-  )
+class NotePadControl extends React.component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <></>;
+  }
 }
 
-export default NotePadControl
+export default NotePadControl;
+
+// notes:
+  // 9/16/22
+    // setup class component
+    
