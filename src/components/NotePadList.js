fee44ba0e94212edd5b169f4c6cc37b21a30961c
Yazmin Perez <zoeperez333@gmail.com>
2022-09-16 20:17:47 -0700

    setup props & add fake data

--- src/components/NotePadList.js
+++ src/components/NotePadList.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import NotePad from "./NotePad";
 
 function NotePadList() {
+  const mainNotePadList = [
+    {
+      title: "Gear",
+      body: "tent, backpack, tarp",
+    },
+    {
+      title: "food",
+      body: "protein bars, oatmeal, trail mix",
+    },
+  ];
+
   return (
-    <div>NotePadList</div>
-  )
+    <>
+      <h2>List</h2>
+      {mainNotePadList.map((note, index) => (
+        <NotePad title={note.title} body={note.body} key={index} />
+      ))}
+    </>
+  );
 }
 
-export default NotePadList
+export default NotePadList;
+
+// notes:
+  //  add <NotePad />
+  // set up props ex: title={}
+  // add fake data -> mainNotePadList
