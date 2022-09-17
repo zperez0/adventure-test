a1b1c1196d723644a6a55fceb1356477932090c6
Yazmin Perez <zoeperez333@gmail.com>
2022-09-16 18:20:50 -0700

    add firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -8,9 +8,10 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
-}
+};
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export default db;
+export { db };
