3dc96f5bc6ef0ddd528ad0d9403fa49c6511009b
Yazmin Perez <zoeperez333@gmail.com>
2022-09-16 19:53:18 -0700

    import NotePadList

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import NotePadList from './NotePadList';
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <NotePadList />
     </React.Fragment>
   );
 }
